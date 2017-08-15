f63b531350b4d7d853daa85a5fb81731fadb4c1b
Tong Sun <suntong@cpan.org>
2017-08-14 22:58:32 -0400

    - [+] similarity check working

--- fsimilarCLICmd.go
+++ fsimilarCLICmd.go
@@ -11,9 +11,13 @@ import (
 	"fmt"
 	"io"
 	"math/rand"
+	"os"
 	"path/filepath"
 	"regexp"
 	"time"
+
+	"github.com/go-dedup/simhash"
+	"github.com/go-dedup/simhash/sho"
 
 	"github.com/mkideal/cli"
 )
@@ -51,22 +55,41 @@ func fSimilar(cin io.Reader) error {
 	rand.Seed(time.Now().UTC().UnixNano())
 	//tmpfile := fmt.Sprintf("%s.%d", file, 99999999-rand.Int31n(90000000))
 
+	oracle := sho.NewOracle()
+	sh := simhash.NewSimhash()
+	r := Opts.Distance
+
+	// read input line by line
 	scanner := bufio.NewScanner(cin)
 	for scanner.Scan() {
 		file := FileT{}
 		fn := scanner.Text()
 
+		// == Gather file info
 		if Opts.SizeGiven {
 			_, err := fmt.Sscan(fn, &file.Size)
 			abortOn("Parsing file size", err)
-			il := regexp.MustCompile(`^ *\d+ *(.*)$`).FindStringSubmatchIndex(fn)
+			il := regexp.MustCompile(`^ *\d+\s+(.*)$`).FindStringSubmatchIndex(fn)
 			//fmt.Println(il)
 			fn = fn[il[2]:]
+		} else {
+			s, err := os.Stat(fn)
+			warnOn("Get file size", err)
+			file.Size = int(s.Size())
 		}
 		p, n := filepath.Split(fn)
 		file.Dir, file.Name, file.Ext = p, Basename(n), filepath.Ext(n)
-		fmt.Printf("  d='%s', n='%s', e='%s', s='%d'\n",
-			file.Dir, file.Name, file.Ext, file.Size)
+		verbose(1, " n='%s', e='%s', s='%d', d='%s'",
+			file.Name, file.Ext, file.Size, file.Dir)
+
+		// == Check similarity
+		hash := sh.GetSimhash(sh.NewWordFeatureSet([]byte(file.Name)))
+		if h, d, seen := oracle.Find(hash, r); seen == true {
+			verbose(1, "=: Simhash of %x ignored for %x (%d).", hash, h, d)
+		} else {
+			oracle.See(hash)
+			verbose(1, "+: Simhash of %x added.", hash)
+		}
 	}
 
 	return scanner.Err()
--- fsimilarMain.go
+++ fsimilarMain.go
@@ -13,6 +13,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/labstack/gommon/color"
 	"github.com/mkideal/cli"
 )
 
@@ -57,6 +58,14 @@ func main() {
 //==========================================================================
 // support functions
 
+// Abs returns the absolute value of x.
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
 // Basename returns the file name without extension.
 func Basename(s string) string {
 	n := strings.LastIndexByte(s, '.')
@@ -66,17 +75,29 @@ func Basename(s string) string {
 	return s
 }
 
+func warning(m string) {
+	fmt.Fprintf(os.Stderr, "[%s] %s: %s\n", progname, color.Yellow("Warning"), m)
+}
+
+func warnOn(errCase string, e error) {
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "[%s] %s, %s: %v\n",
+			color.White(progname), color.Yellow("Error"), errCase, e)
+	}
+}
+
 // abortOn will quit on anticipated errors gracefully without stack trace
 func abortOn(errCase string, e error) {
 	if e != nil {
-		fmt.Fprintf(os.Stderr, "[%s] %s error: %v\n", progname, errCase, e)
+		fmt.Fprintf(os.Stderr, "[%s] %s, %s: %v\n",
+			color.White(progname), color.Red("Error"), errCase, e)
 		os.Exit(1)
 	}
 }
 
 // verbose will print info to stderr according to the verbose level setting
-func verbose(levelSet, levelNow int, format string, args ...interface{}) {
-	if levelNow >= levelSet {
+func verbose(levelSet int, format string, args ...interface{}) {
+	if Opts.Verbose >= levelSet {
 		fmt.Fprintf(os.Stderr, "["+progname+"] "+format+"\n", args...)
 	}
 }
